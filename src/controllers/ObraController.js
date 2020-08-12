4e58da686d76676d7b37f8fee44e96f87b914ef4
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-12 19:54:03 -0300

    PUT and DELETE by id

--- src/controllers/ObraController.js
+++ src/controllers/ObraController.js
@@ -12,5 +12,15 @@ module.exports = {
     async create(req, res) {
         const obra = await Obra.create(req.body);
         return res.json(obra);
+    },
+
+    async update(req, res) {
+        const obra = await Obra.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        return res.json(obra);
+    },
+
+    async delete(req, res) {
+        await Obra.findByIdAndDelete(req.params.id); 
+        return res.send();
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const ObraController = require('./controllers/ObraController')
 
 routes.get('/obras', ObraController.findAll);
 routes.post('/obras', ObraController.create);
+routes.put('/obras/:id', ObraController.update);
+routes.delete('/obras/:id', ObraController.delete);
 
 module.exports = routes;
